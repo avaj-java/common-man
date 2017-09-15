cca7b01d681dcf7ae13752db56dd8be82bf88f5d
souljungkim <souljungkim@gmail.com>
2017-09-15 14:27:49 +0900

    Update Anootations

--- src/main/java/jaemisseo/man/annotation/QueryPageNumber.groovy
+++ src/main/java/jaemisseo/man/annotation/QueryPageNumber.groovy
@@ -1,8 +1,15 @@
 package jaemisseo.man.annotation
+
+import java.lang.annotation.ElementType
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy
+import java.lang.annotation.Target
+
 /**
  * Created by sujkim on 2017-02-22.
  */
-
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
 @interface QueryPageNumber {
 
 }
--- src/main/java/jaemisseo/man/annotation/QueryTableSetup.groovy
+++ src/main/java/jaemisseo/man/annotation/QueryTableSetup.groovy
@@ -6,9 +6,16 @@ import java.lang.annotation.RetentionPolicy
 import java.lang.annotation.Target
 
 /**
- * Created by sujkim on 2017-02-22.
+ * Created with IntelliJ IDEA.
+ * User: sujkim
+ * Date: 10/27/16
+ * Time: 11:26 PM
+ * To change this template use File | Settings | File Templates.
  */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
-@interface QueryPageSize {
+public @interface QueryTableSetup {
+
+    String value() default "DEFAULT";
+
 }
