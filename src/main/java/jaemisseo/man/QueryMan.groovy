0766f2d1ec4a2df9d5836603da59499249808d4b
souljungkim <souljungkim@gmail.com>
2018-03-12 18:59:29 +0900

    Update QueryMan Log Printing

--- src/main/java/jaemisseo/man/QueryMan.groovy
+++ src/main/java/jaemisseo/man/QueryMan.groovy
@@ -4,6 +4,7 @@ import groovy.sql.BatchingPreparedStatementWrapper
 import groovy.sql.GroovyRowResult
 import groovy.sql.Sql
 import jaemisseo.man.annotation.*
+import jaemisseo.man.util.ConnectionGenerator
 import oracle.sql.TIMESTAMP
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -31,141 +32,6 @@ class QueryMan {
 
     static final String DESC = "DESC"
     static final String ASC = "ASC"
-
-    class ConnectionGenerator{
-
-        ConnectionGenerator(){}
-
-        ConnectionGenerator(Map<String, String> map){
-            setDatasource(map)
-        }
-
-        static final String ORACLE = "ORACLE"           // 'jdbc:oracle:thin:' 'oracle.jdbc.driver.OracleDriver'
-        static final String TIBERO = "TIBERO"           // 'jdbc:tibero:thin:' 'com.tmax.tibero.jdbc.TbDriver'
-        static final String MYSQL = "MYSQL"             // 'jdbc:mysql:' 'com.mysql.jdbc.Driver'
-        static final String MSSQL = "MSSQL"             // 'jdbc:sqlserver:' 'com.microsoft.sqlserver.jdbc.SQLServerDriver'
-        static final String INFORMIX = "INFORMIX"       // 'jdbc:informix-sqli:' 'com.informix.jdbc.IfxDriver'
-        static final String ALTIBASE = "ALTIBASE"       // 'jdbc:Altibase:' 'Altibase.jdbc.driver.AltibaseDriver'
-        static final String SYBASE = "SYBASE"           // 'jdbc:sybase:Tds:' 'com.sybase.jdbc3.jdbc.SybDriver'
-        static final String IBM_DB = "IBM_DB"           // 'jdbc:db2:' 'com.ibm.db2.jcc.DB2Driver'
-
-        String vendor, user, password, ip, port, db, url, driver
-
-        ConnectionGenerator init(){
-            vendor=null; user=null; password=null; ip=null; port=null; db=null; url=null; driver=null;
-            return this
-        }
-
-        ConnectionGenerator setDatasource(Map map){
-            vendor  = map['vendor']
-            user    = map['user']
-            password= map['password']
-            ip      = map['ip']
-            port    = map['port']
-            db      = map['db']
-            url     = map['url']
-            driver  = map['driver']
-            return this
-        }
-
-        Map<String, String> generateDataBaseInfoMap(){
-            Map o = [
-                vendor  : vendor ?: ORACLE,
-                user    : user,
-                password: password,
-                ip      : ip ?: "127.0.0.1",
-                port    : port ?: "1521",
-                db      : db ?: "orcl",
-            ]
-            o['url']    = url ?: getURLProtocol(o.vendor, o.ip, o.port, o.db)
-            o['driver'] = driver ?: getDriverName(o.vendor)
-            return o
-        }
-
-        Connection generate(Map map){
-            setDatasource(map)
-            return generate()
-        }
-
-        Connection generate(){
-            Sql sql = generateSqlInstance()
-            return sql.getConnection()
-        }
-
-        Sql generateSqlInstance(){
-            Map<String, String> map = generateDataBaseInfoMap()
-            return Sql.newInstance(map.url, map.user, map.password, map.driver)
-        }
-
-        String getDriverName(String vendor){
-            String driver
-            switch (vendor?.toUpperCase()){
-                case ORACLE:
-                    driver = "oracle.jdbc.driver.OracleDriver"
-                    break
-                case TIBERO:
-                    driver = "com.tmax.tibero.jdbc.TbDriver"
-                    break
-                case MYSQL:
-                    driver = 'com.mysql.jdbc.Driver'
-                    break
-                case MSSQL:
-                    driver = 'com.microsoft.sqlserver.jdbc.SQLServerDriver'
-                    break
-                case INFORMIX:
-                    driver = 'com.informix.jdbc.IfxDriver'
-                    break
-                case ALTIBASE:
-                    driver = 'Altibase.jdbc.driver.AltibaseDriver'
-                    break
-                case SYBASE:
-                    driver = 'com.sybase.jdbc3.jdbc.SybDriver'
-                    break
-                case IBM_DB:
-                    driver = 'com.ibm.db2.jcc.DB2Driver'
-                    break
-                default:
-                    driver = "oracle.jdbc.driver.OracleDriver"
-                    break
-            }
-            return driver
-        }
-
-        String getURLProtocol(String vendor, String ip, String port, String db) {
-            String URLProtocol
-            switch (vendor?.toUpperCase()) {
-                case ORACLE:
-                    URLProtocol = "jdbc:oracle:thin:@${ip}:${port}:${db}"
-                    break
-                case TIBERO:
-                    URLProtocol = "jdbc:tibero:thin:@${ip}:${port}:${db}"
-                    break
-                case MYSQL:
-                    URLProtocol = "jdbc:mysql:${ip}:${port}:${db}"
-                    break
-                case MSSQL:
-                    URLProtocol = "jdbc:sqlserver:${ip}:${port}:${db}"
-                    break
-                case INFORMIX:
-                    URLProtocol = "jdbc:informix-sqli:${ip}:${port}:${db}"
-                    break
-                case ALTIBASE:
-                    URLProtocol = "jdbc:Altibase:${ip}:${port}:${db}"
-                    break
-                case SYBASE:
-                    URLProtocol = "jdbc:sybase:Tds:${ip}:${port}:${db}"
-                    break
-                case IBM_DB:
-                    URLProtocol = "jdbc:db2:${ip}:${port}:${db}"
-                    break
-                default:
-                    URLProtocol = "jdbc:oracle:thin:@${ip}:${port}:${db}"
-                    break
-            }
-            return URLProtocol
-        }
-    }
-
 
     //No Clearable
     Connection recentConn
@@ -1748,26 +1614,39 @@ class QueryMan {
      * 본 QueryManager를 호출한 Class명으로 로그(QueryName, Query, Parameters)출력
      *************************/
     void log(String logContent){
-        log('', logContent)
+        log('QueryMan', logContent)
     }
 
     void log(String logName, String logContent){
+        log(logName, [logContent])
+    }
+
+    void log(String logName, List<String> logContentList){
         StackTraceElement[] stacks = Thread.currentThread().getStackTrace()
         for (int i=0; i<stacks.length; i++){
+            String fileName = stacks[i].getFileName()
+            String classPath = stacks[i].getClassName()
+            String className = classPath?.split('[.]')?.last()
+            String methodName = stacks[i].getMethodName()
+            int lineNumber = stacks[i].getLineNumber()
             if (i > 1
-                    && !stacks[i].getClassName().startsWith(packagePath)
-                    && !stacks[i].getClassName().startsWith('java.lang')
-                    && !stacks[i].getClassName().startsWith('sun.reflect')
-                    && !stacks[i].getClassName().startsWith('org.codehaus.groovy')){
-                String className = stacks[i].getClassName()
-                String methodName = stacks[i].getMethodName()
-                int lineNumber = stacks[i].getLineNumber()
-                String logString = "[${methodName}:${lineNumber}] - <${logName}> \n${logContent}"
-                if (modeJUnitTest){
-                    println logString
-                }else{
-                    logger = LoggerFactory.getLogger(className)
-                    logger.debug logString
+                    && !classPath.startsWith(packagePath)
+                    && !classPath.startsWith('java.lang')
+                    && !classPath.startsWith('sun.reflect')
+                    && !classPath.startsWith('org.codehaus.groovy')
+                    && !classPath.startsWith('groovy.lang')
+                    && !classPath.contains('$')
+                    && !fileName.startsWith('QueryMan')){
+
+                logger = LoggerFactory.getLogger("${classPath}.${methodName}")
+
+                if (logName)
+                    logger.debug "<${logName}> ${className}.${methodName}:${lineNumber}"
+
+                logContentList.each{
+                    if (it){
+                        logger.debug it
+                    }
                 }
                 break
             }
@@ -1778,42 +1657,45 @@ class QueryMan {
         String queryLog = ''
         String paramLog = ''
         String resultLog = ''
-        //Query Log
-        if (modeFlattenQueryLog)
-            query = query.replaceAll("\n+", " ").replaceAll("\r+", " ").replaceAll("\t+", " ").replaceAll("\\s+", " ")
-        queryLog = " - Query: ${query}\n"
-        //Parameter Log
-        if (params)
-            paramLog = " - Parameters: ${params.toString()}\n"
-        //Result Log
-        if (result){
-            if (result instanceof List || result instanceof Map)
-                resultLog = " - Size: ${result.size()}"
-            else
-                resultLog = " - Result: ${result}"
+        //Making Log - Query
+        if (query){
+            if (modeFlattenQueryLog)
+                query = query.replaceAll("\n+", " ").replaceAll("\r+", " ").replaceAll("\t+", " ").replaceAll("\\s+", " ")
+            queryLog = "==>  Preparing: ${query}"
         }
-        log('Query', "${queryLog}${paramLog}${resultLog}")
+        //Making Log - Parameter
+        if (params){
+            paramLog = "==> Parameters: ${params.toString()}\n"
+        }
+        //Making Log - Result
+        if (result){
+            resultLog = (result instanceof List || result instanceof Map) ? "<==       Size: ${result.size()}" : "<==     Result: ${result}"
+        }
+        log('QueryMan', [queryLog, paramLog, resultLog])
     }
 
     void logResultForExecute(String query, List params, def result){
         String queryLog = ''
         String paramLog = ''
         String resultLog = ''
-        //Query Log
-        if (modeFlattenQueryLog)
-            query = query.replaceAll("\n+", " ").replaceAll("\r+", " ").replaceAll("\t+", " ").replaceAll("\\s+", " ")
-        queryLog = " - Query: ${query}\n"
-        //Parameter Log
-        if (params)
-            paramLog = " - Parameters: ${params.toString()}\n"
-        //Result Log
+        //Making Log - Query
+        if (query){
+            if (modeFlattenQueryLog)
+                query = query.replaceAll("\n+", " ").replaceAll("\r+", " ").replaceAll("\t+", " ").replaceAll("\\s+", " ")
+            queryLog = "==>  Preparing: ${query}\n"
+        }
+        //Making Log - Parameter
+        if (params){
+            paramLog = "==> Parameters: ${params.toString()}\n"
+        }
+        //Making Log - Result
         if (result){
             if (result instanceof List || result instanceof Array)
-                resultLog = " - Success: ${result.findAll{ it == -2 }.size()} / ${result.size()}"
+                resultLog = "<==    Success: ${result.findAll{ it == -2 }.size()} / ${result.size()}"
             else if (result instanceof Boolean)
-                resultLog = " - Success: ${result}"
+                resultLog = "<==    Success: ${result}"
         }
-        log('Query', "${queryLog}${paramLog}${resultLog}")
+        log('QueryMan', [queryLog, paramLog, resultLog])
     }
 
     /**
